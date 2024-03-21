e5832e0dcd268ab8afb74c3badff4b1bf4958de3
Z4xk3r0th <30985576+TheProdigyLeague@users.noreply.github.com>
2024-03-21 14:53:56 -0500

    Update main_menu_0.2.py
    remove the usual duplicate scripts from dumbass devs

--- paywalls.yml/water_levels.yml/deprecation.yml/main_menu_0.2.py
+++ paywalls.yml/water_levels.yml/deprecation.yml/main_menu_0.2.py
@@ -356,12 +356,14 @@ def get_graphql_response(*, settings: Settings, query: str, after: Union[str, No
         "comment_id": comment_id,
         "body": body,
     }
+`
     response = httpx.post(
         github_graphql_url,
         headers=headers,
         timeout=settings.httpx_timeout,
         json={"query": query, "variables": variables, "operationName": "Q"},
     )
+
     if response.status_code != 200:
         logging.error(
             f"Response was not 200, after: {after}, category_id: {category_id}"
@@ -390,32 +392,7 @@ if response.status_code != 200:
   return data
 `
 ~
-    variables = {
-        "after": after,
-        "category_id": category_id,
-        "discussion_number": discussion_number,
-        "discussion_id": discussion_id,
-        "comment_id": comment_id,
-        "body": body,
-    }
-    response = httpx.post(
-        github_graphql_url,
-        headers=headers,
-        timeout=settings.httpx_timeout,
-        json={"query": query, "variables": variables, "operationName": "Q"},
-    )
-    if response.status_code != 200:
-        logging.error(
-            f"Response was not 200, after: {after}, category_id: {category_id}"
-        )
-        logging.error(response.text)
-        raise RuntimeError(response.text)
-    data = response.json()
-    if "errors" in data:
-        logging.error(f"Errors in response, after: {after}, category_id: {category_id}")
-        logging.error(response.text)
-        raise RuntimeError(response.text)
-    return cast(Dict[str, Any], data)
+# dumbass devs literally copying and recycling old scripts from different files.
 `
 def get_graphql_translation_discussions(*, settings: Settings):
     data = get_graphql_response(
