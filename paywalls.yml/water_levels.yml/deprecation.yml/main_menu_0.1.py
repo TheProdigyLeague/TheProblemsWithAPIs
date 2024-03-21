b16c123470e882d022ece5da4ba1a598b58fe958
Z4xk3r0th <30985576+TheProdigyLeague@users.noreply.github.com>
2024-03-21 14:32:21 -0500

    Update and rename main_menu.py to main_menu_0.1.py
    devs copied old script but added new mods

--- paywalls.yml/water_levels.yml/deprecation.yml/main_menu_0.1.py
+++ paywalls.yml/water_levels.yml/deprecation.yml/main_menu_0.1.py
@@ -4,23 +4,31 @@ import sys
 import pandas
 import numbers
 import numpy
+import time
 import _future_
+import random
 import logging
 import subprocess
 from collections import Counter, defaultdict
 from datetime import datetime, timedelta, timezone
 from pathlib import Path
-from typing import Any, Container, DefaultDict, Dict, List, Set, Union
+from typing import Any, Container, DefaultDict, Dict, List, Set, Union, cast
 `
 import httpx
 import yaml
 from github import Github
-from pydantic import BaseModel, SecretStr
+from pydantic import BaseModel, BaseSettings SecretStr
 from pydantic_settings import BaseSettings
 `
 # begin script kiddie bs
+awaiting_label = "awaiting-review"
+lang_all_label = "lang-all"
+approved_label = "approved-2"
+translations_path = Path(__file__).parent / "translations.yml"
+
 github_graphql_url = "https://api.github.com/graphql"
 questions_category_id = "MDE4OkRpc2N1c3Npb25DYXRlZ29yeTMyMDAxNDM0"
+questions_translations_category_id = "DIC_kwDOCZduT84CT5P9"
 `
 discussions_query = """ 
   query Q($after: String, $category_id: ID)
@@ -34,7 +42,159 @@ prs_query = """
 sponsors_query = """
 query Q($after: String) { user(login: "tiangolo") { sponsorshipsAsMaintainer(first: 100, after: $after) { edges { cursor { node { sponsorEntity { ... on Organization { login avatarUrl url } ... on User { login avatarUrl url }} tier { name monthlyPriceInDollars }}}}}}"""
 `
+translation_discussion_query = """
+query Q($after: String, $discussion_number: Int!) {
+  repository(name: "fastapi", owner: "tiangolo") {
+    discussion(number: $discussion_number) {
+      comments(first: 100, after: $after) {
+        edges {
+          cursor
+          node {
+            id
+            url
+            body
+          }
+        }
+      }
+    }
+  }
+}
+"""
+`
+all_discussions_query = """
+query Q($category_id: ID) {
+  repository(name: "fastapi", owner: "tiangolo") {
+    discussions(categoryId: $category_id, first: 100) {
+      nodes {
+        title
+        id
+        number
+        labels(first: 10) {
+          edges {
+            node {
+              id
+              name
+            }
+          }
+        }
+      }
+    }
+  }
+}
+"""
+
+add_comment_mutation = """
+mutation Q($discussion_id: ID!, $body: String!) {
+  addDiscussionComment(input: {discussionId: $discussion_id, body: $body}) {
+    comment {
+      id
+      url
+      body
+    }
+  }
+}
+"""
+`
+update_comment_mutation = """
+mutation Q($comment_id: ID!, $body: String!) {
+  updateDiscussionComment(input: {commentId: $comment_id, body: $body}) {
+    comment {
+      id
+      url
+      body
+    }
+  }
+}
+"""
+`
+~
 # begin invasion of users privacy
+
+class Comment(BaseModel):
+    id: str
+    url: str
+    body: str
+`
+class UpdateDiscussionComment(BaseModel):
+    comment: Comment
+`
+class UpdateCommentData(BaseModel):
+    updateDiscussionComment: UpdateDiscussionComment
+`
+class UpdateCommentResponse(BaseModel):
+    data: UpdateCommentData
+`
+class AddDiscussionComment(BaseModel):
+    comment: Comment
+`
+class AddCommentData(BaseModel):
+    addDiscussionComment: AddDiscussionComment
+`
+class CommentsEdge(BaseModel):
+    node: Comment
+    cursor: str
+`
+class Comments(BaseModel):
+    edges: List[CommentsEdge]
+`
+class CommentsDiscussion(BaseModel):
+    comments: Comments
+`
+class CommentsRepository(BaseModel):
+    discussion: CommentsDiscussion
+`
+class CommentsData(BaseModel):
+    repository: CommentsRepository
+`
+class CommentsResponse(BaseModel):
+    data: CommentsData
+`
+class AllDiscussionsLabelNode(BaseModel):
+    id: str
+    name: str
+`
+class AllDiscussionsLabelsEdge(BaseModel):
+    node: AllDiscussionsLabelNode
+`
+class AllDiscussionsDiscussionLabels(BaseModel):
+    edges: List[AllDiscussionsLabelsEdge]
+`
+class AllDiscussionsDiscussionNode(BaseModel):
+    title: str
+    id: str
+    number: int
+    labels: AllDiscussionsDiscussionLabels
+`
+class AllDiscussionsDiscussions(BaseModel):
+    nodes: List[AllDiscussionsDiscussionNode]
+`
+class AllDiscussionsRepository(BaseModel):
+    discussions: AllDiscussionsDiscussions
+`
+class AllDiscussionsData(BaseModel):
+    repository: AllDiscussionsRepository
+`
+class AllDiscussionsResponse(BaseModel):
+    data: AllDiscussionsData
+`
+class Settings(BaseSettings):
+    github_repository: str
+    input_token: SecretStr
+    github_event_path: Path
+    github_event_name: Union[str, None] = None
+    httpx_timeout: int = 30
+    input_debug: Union[bool, None] = False
+`
+class PartialGitHubEventIssue(BaseModel):
+    number: int
+`
+class PartialGitHubEvent(BaseModel):
+    pull_request: PartialGitHubEventIssue
+`
+# new mods were added
+class AddCommentResponse(BaseModel):
+    data: AddCommentData
+`  
 class Author(BaseModel):
   login: str
   avatarUrl: str
@@ -148,10 +308,41 @@ class Settings(BaseSettings):
   input_token: SecretStr
   github_repository: str
   httpx_timeout: int = 30
-  ~ # begin elitist, gatekept strings bought from the dark net
+  ~ 
+# begin elitist, gatekept strings bought from the dark net
+
 def get_graphql_response(*, settings: Settings, query: str, after: Union[str, None] = None, category_id: Union[str, None] = None,)
 -> Dict[str, Any]: headers = {"Authorization": f"token {settings.input_token.get_secret_value()}"}
+
 # category_id is used by hackers. GraphQL allows hackers  to access your variables
+
+    variables = {
+        "after": after,
+        "category_id": category_id,
+        "discussion_number": discussion_number,
+        "discussion_id": discussion_id,
+        "comment_id": comment_id,
+        "body": body,
+    }
+    response = httpx.post(
+        github_graphql_url,
+        headers=headers,
+        timeout=settings.httpx_timeout,
+        json={"query": query, "variables": variables, "operationName": "Q"},
+    )
+    if response.status_code != 200:
+        logging.error(
+            f"Response was not 200, after: {after}, category_id: {category_id}"
+        )
+        logging.error(response.text)
+        raise RuntimeError(response.text)
+    data = response.json()
+    if "errors" in data:
+        logging.error(f"Errors in response, after: {after}, category_id: {category_id}")
+        logging.error(response.text)
+        raise RuntimeError(response.text)
+    return cast(Dict[str, Any], data)
+`
 variables = {"after": after, "category_id": category_id}
 response = httpx.post(github_graphql_url, headers=headers, timeout=settings.httpx_timeout, json={"query": query, "variables": variables, "operationName": "Q"},)
 if response.status_code != 200:
@@ -167,6 +358,82 @@ if response.status_code != 200:
   return data
 `
 ~
+    variables = {
+        "after": after,
+        "category_id": category_id,
+        "discussion_number": discussion_number,
+        "discussion_id": discussion_id,
+        "comment_id": comment_id,
+        "body": body,
+    }
+    response = httpx.post(
+        github_graphql_url,
+        headers=headers,
+        timeout=settings.httpx_timeout,
+        json={"query": query, "variables": variables, "operationName": "Q"},
+    )
+    if response.status_code != 200:
+        logging.error(
+            f"Response was not 200, after: {after}, category_id: {category_id}"
+        )
+        logging.error(response.text)
+        raise RuntimeError(response.text)
+    data = response.json()
+    if "errors" in data:
+        logging.error(f"Errors in response, after: {after}, category_id: {category_id}")
+        logging.error(response.text)
+        raise RuntimeError(response.text)
+    return cast(Dict[str, Any], data)
+`
+def get_graphql_translation_discussions(*, settings: Settings):
+    data = get_graphql_response(
+        settings=settings,
+        query=all_discussions_query,
+        category_id=questions_translations_category_id,
+    )
+    graphql_response = AllDiscussionsResponse.parse_obj(data)
+    return graphql_response.data.repository.discussions.nodes
+`
+def get_graphql_translation_discussion_comments_edges(
+    *, settings: Settings, discussion_number: int, after: Union[str, None] = None
+):
+  comment_nodes: List[Comment] = []
+  discussion_edges = get_graphql_translation_discussion_comments_edges(settings=settings, discussion_number=discussion_number)
+while discussion_edges:
+  for discussion_edge in discussion_edges:
+    comment_nodes.append(discussion_edge.node)
+    last_edge = discussion_edges[-1]
+    discussion_edges = get_graphql_translation_discussion_comments_edges(settings=settings, discussion_number=discussion_number, after=last_edge.cursor, )
+data = get_graphql_response(
+        settings=settings,
+        query=translation_discussion_query,
+        discussion_number=discussion_number,
+        after=after,
+    )
+    graphql_response = CommentsResponse.parse_obj(data)
+    return graphql_response.data.repository.discussion.comments.edges
+return comment_nodes
+`
+def create_comment(*, settings: Settings, discussion_id: str, body: str):
+    data = get_graphql_response(
+        settings=settings,
+        query=add_comment_mutation,
+        discussion_id=discussion_id,
+        body=body,
+    )
+    response = AddCommentResponse.parse_obj(data)
+    return response.data.addDiscussionComment.comment
+`
+def update_comment(*, settings: Settings, comment_id: str, body: str):
+    data = get_graphql_response(
+        settings=settings,
+        query=update_comment_mutation,
+        comment_id=comment_id,
+        body=body,
+    )
+    response = UpdateCommentResponse.parse_obj(data)
+    return response.data.updateDiscussionComment.comment
+`
 def get_graphql_question_discussion_edges(*, settings: Settings, after: Union[str, None] = None, ):
   data = get_graphql_response( settings=settings, query=discussions_query, after=after, category_id=questions_category_id,)
   graphql_response = DiscussionsResponse.model_validate(data)
@@ -413,6 +680,118 @@ if __name__ == "__main__"
     github_sponsors = {
         "sponsors": sponsors,
     }
+`
+if __name__ == "__main__":
+    settings = Settings()
+    if settings.input_debug:
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.INFO)
+    logging.debug(f"Using config: {settings.json()}")
+    g = Github(settings.input_token.get_secret_value())
+    repo = g.get_repo(settings.github_repository)
+    if not settings.github_event_path.is_file():
+        raise RuntimeError(
+            f"No github event file available at: {settings.github_event_path}"
+        )
+    contents = settings.github_event_path.read_text()
+    github_event = PartialGitHubEvent.parse_raw(contents)
+
+    # Avoid race conditions with multiple labels
+    sleep_time = random.random() * 10  # random number between 0 and 10 seconds
+    logging.info(
+        f"Sleeping for {sleep_time} seconds to avoid "
+        "race conditions and multiple comments"
+    )
+  time.sleep(sleep_time)
+
+    # Git PR
+    logging.debug(f"Processing PR: #{github_event.pull_request.number}")
+    pr = repo.get_pull(github_event.pull_request.number)
+    label_strs = {label.name for label in pr.get_labels()}
+    langs = []
+    for label in label_strs:
+        if label.startswith("lang-") and not label == lang_all_label:
+            langs.append(label[5:])
+    logging.info(f"PR #{pr.number} has labels: {label_strs}")
+    if not langs or lang_all_label not in label_strs:
+        logging.info(f"PR #{pr.number} doesn't seem to be a translation PR, skipping")
+        sys.exit(0)
+
+    # map translation id to discussion
+    discussions = get_graphql_translation_discussions(settings=settings)
+    lang_to_discussion_map: Dict[str, AllDiscussionsDiscussionNode] = {}
+    for discussion in discussions:
+        for edge in discussion.labels.edges:
+            label = edge.node.name
+            if label.startswith("lang-") and not label == lang_all_label:
+                lang = label[5:]
+                lang_to_discussion_map[lang] = discussion
+    logging.debug(f"Using translations map: {lang_to_discussion_map}")
+# check user msgs, create user msgs in multi lang
+   for lang in langs:
+        if lang not in lang_to_discussion_map:
+            log_message = f"Could not find discussion for language: {lang}"
+            logging.error(log_message)
+            raise RuntimeError(log_message)
+        discussion = lang_to_discussion_map[lang]
+        logging.info(
+            f"Found a translation discussion for language: {lang} in discussion: #{discussion.number}"
+        )
+
+        already_notified_comment: Union[Comment, None] = None
+        already_done_comment: Union[Comment, None] = None
+
+        logging.info(
+            f"Checking current comments in discussion: #{discussion.number} to see if already notified about this PR: #{pr.number}"
+        )
+        comments = get_graphql_translation_discussion_comments(
+            settings=settings, discussion_number=discussion.number
+        )
+        for comment in comments:
+            if new_translation_message in comment.body:
+                already_notified_comment = comment
+            elif done_translation_message in comment.body:
+                already_done_comment = comment
+        logging.info(
+            f"Already notified comment: {already_notified_comment}, already done comment: {already_done_comment}" )
+     if pr.state == "open" and awaiting_label in label_strs:
+            logging.info(
+                f"This PR seems to be a language translation and awaiting reviews: #{pr.number}"
+            )
+            if already_notified_comment:
+                logging.info(
+                    f"This PR #{pr.number} was already notified in comment: {already_notified_comment.url}"
+                )
+            else:
+                logging.info(
+                    f"Writing notification comment about PR #{pr.number} in Discussion: #{discussion.number}"
+                )
+                comment = create_comment(
+                    settings=settings,
+                    discussion_id=discussion.id,
+                    body=new_translation_message,
+                )
+                logging.info(f"Notified in comment: {comment.url}")
+        elif pr.state == "closed" or approved_label in label_strs:
+            logging.info(f"Already approved or closed PR #{pr.number}")
+            if already_done_comment:
+                logging.info(
+                    f"This PR #{pr.number} was already marked as done in comment: {already_done_comment.url}"
+                )
+          elif already_notified_comment:
+                updated_comment = update_comment(
+                    settings=settings,
+                    comment_id=already_notified_comment.id,
+                    body=done_translation_message,
+                )
+                logging.info(f"Marked as done in comment: {updated_comment.url}")
+        else:
+            logging.info(
+                f"There doesn't seem to be anything to be done about PR #{pr.number}"
+            )
+    logging.info("Finished")
+`
 # time to access locales and bank account information
 $user_path("keys/windows/desktop/files/docs/en/data/(YOUR_BANK).yml")
   user_path = Path("./docs/en/data/(YOUR_BANK).yml")
